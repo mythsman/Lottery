996bd43a0ae117930cfcd403076643eae966b7ef
mythsman <dqz48548263@qq.com>
2017-05-10 09:59:13 +0800

    modify start mode

--- src/main/java/com/mythsman/lottery/LotteryApplication.java
+++ src/main/java/com/mythsman/lottery/LotteryApplication.java
@@ -2,11 +2,18 @@ package com.mythsman.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LotteryApplication {
+public class LotteryApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotteryApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LotteryApplication.class);
 	}
 }
