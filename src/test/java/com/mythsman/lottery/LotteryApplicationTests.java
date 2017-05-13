9c5f0f973810846afe1dce5e16d39a6dfe5bb2d5
mythsman <dqz48548263@qq.com>
2017-05-14 00:48:57 +0800

    Add pictures.

--- src/test/java/com/mythsman/lottery/LotteryApplicationTests.java
+++ src/test/java/com/mythsman/lottery/LotteryApplicationTests.java
@@ -3,11 +3,11 @@ package com.mythsman.lottery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
 public class LotteryApplicationTests {
 
 	@Test
